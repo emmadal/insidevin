ea3c7b30d7283f2c6fd46d7d6ee2632e70322206
emmadal <emmanueldalougou@gmail.com>
2023-12-01 11:38:34 +0000

    change regex rules

--- app/schema.ts
+++ app/schema.ts
@@ -4,7 +4,7 @@ export const vinSchema = z.object({
   vin: z
     .string()
     .min(17, "VIN length less than 17 characters")
-    .max(17, "VIN CAR can contain only 17 digits.")
-    .regex(/^[1-9]\d{14,17}$/, "Only number is allowed for VIN CAR")
+    .max(17, "VIN CAR can contain only 17 characters.")
+    .regex(/^[A-Za-z0-9]+$/, "Only alphabetics and numbers are allowed for VIN CAR")
     .trim(),
 });
