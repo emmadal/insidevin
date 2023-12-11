f43820d24e4c13b55ca35b8f1969e23667fe8213
emmadal <emmanueldalougou@gmail.com>
2023-12-11 16:59:30 +0000

    reajust layout for dashboard

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -3,9 +3,9 @@ import ProfileCard from "../components/ProfileCard.tsx";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="md:px-8 mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 min-h-screen">
-      <div className="w-full md:grid md:grid-cols-3 gap-8 mt-20 space-x-16">
-        <div className="col-span-2">{children}</div>
+    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 min-h-screen">
+      <div className="md:grid md:grid-cols-4 gap-3 mt-20 space-x-3">
+        <div className="col-span-3">{children}</div>
         <div className="hidden md:block">
           <ProfileCard />
         </div>
